f78a7bba171618d2cba0270de2ebf40c3040d151
joel.clement <joelclems@gmail.com>
2023-05-24 13:52:34 +0200

    add api test

---
+++ scripts/init_applications_config.sh
@@ -0,0 +1,3 @@
+cp data/apps/geonature/config/geonature_config.toml.sample data/apps/geonature/config/geonature_config.toml
+cp data/apps/taxhub/config/config.py.sample data/apps/taxhub/config/config.py
+cp data/apps/usershub/config/config.py.sample data/apps/usershub/config/config.py
