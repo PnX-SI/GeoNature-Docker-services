17aad653cafbf340e78cc006fe6ff8b082a88c20
jacquesfize <jacques.fize@ecrins-parcnational.fr>
2024-08-07 11:24:30 +0200

    remove TaxHub service

--- init-config.sh
+++ init-config.sh
@@ -6,6 +6,3 @@ fi
 if [ ! -f config/usershub/config.py ]; then
     echo SECRET_KEY = \"$(openssl rand -hex 16)\" > config/usershub/config.py
 fi
-if [ ! -f config/taxhub/config.py ]; then
-    echo SECRET_KEY = \"$(openssl rand -hex 16)\" > config/taxhub/config.py
-fi
