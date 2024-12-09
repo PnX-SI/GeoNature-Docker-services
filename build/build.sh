1b7a090d047621e28f311488dd3dd0dd69f2d10c
VincentCauchois <vincent.cauchois@mnhn.fr>
2024-12-09 14:28:39 +0100

    feat(mtd_sync): add external module mtd_sync for inpn mtd sync
    Add repo `mtd_sync` as a git submodule in "./sources/" folder.
    Checkout `mtd_sync` to `main` branch.
    Checkout `GeoNature` git submodule to `develop` branch with evolutions needed to support `mtd_sync` module.
    Adapt Dockerfile-geonature-backend to add `mtd_sync` module into image used for geonature-backend service.
    Adapt `./build/build.sh` script so that it can be used to create a custom `geonature-backend-extra` Docker image only.

--- build/build.sh
+++ build/build.sh
@@ -9,7 +9,7 @@ export DOCKER_BUILDKIT=1
 source .env
 
 # GN BACKEND WHEELS
-docker build -f sources/GeoNature/backend/Dockerfile -t ${GEONATURE_BACKEND_IMAGE}-wheels --target=wheels sources/GeoNature/
+# docker build -f sources/GeoNature/backend/Dockerfile -t ${GEONATURE_BACKEND_IMAGE}-wheels --target=wheels sources/GeoNature/
 
 # GN BACKEND EXTRA
 docker build \
@@ -18,17 +18,17 @@ docker build \
     -t ${GEONATURE_BACKEND_EXTRA_IMAGE} .
 
 # GN FRONTEND SOURCE
-docker build -f sources/GeoNature/frontend/Dockerfile -t ${GEONATURE_FRONTEND_IMAGE}-source --target=source sources/GeoNature/
+# docker build -f sources/GeoNature/frontend/Dockerfile -t ${GEONATURE_FRONTEND_IMAGE}-source --target=source sources/GeoNature/
 
-# GN FRONTEND NGINX
-docker build -f sources/GeoNature/frontend/Dockerfile -t ${GEONATURE_FRONTEND_IMAGE}-nginx --target=prod-base sources/GeoNature/
+# # GN FRONTEND NGINX
+# docker build -f sources/GeoNature/frontend/Dockerfile -t ${GEONATURE_FRONTEND_IMAGE}-nginx --target=prod-base sources/GeoNature/
 
-# GN FRONTEND EXTRA
-docker build \
-    --build-arg GEONATURE_FRONTEND_IMAGE=${GEONATURE_FRONTEND_IMAGE} \
-    -f ./build/Dockerfile-geonature-frontend \
-    -t ${GEONATURE_FRONTEND_EXTRA_IMAGE} .
+# # GN FRONTEND EXTRA
+# docker build \
+#     --build-arg GEONATURE_FRONTEND_IMAGE=${GEONATURE_FRONTEND_IMAGE} \
+#     -f ./build/Dockerfile-geonature-frontend \
+#     -t ${GEONATURE_FRONTEND_EXTRA_IMAGE} .
 
 
-# UsersHub
-docker build -f sources/UsersHub/Dockerfile -t ${USERSHUB_IMAGE} --target=prod sources/UsersHub/
+# # UsersHub
+# docker build -f sources/UsersHub/Dockerfile -t ${USERSHUB_IMAGE} --target=prod sources/UsersHub/
