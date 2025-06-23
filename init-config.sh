0df00205951dac8d6f77e2685b09017fdef1888f
jacquesfize <jacques.fize@ecrins-parcnational.fr>
2025-06-23 10:57:56 +0200

    make config, data dir optional + make network name customisable

--- init-config.sh
+++ init-config.sh
@@ -1,12 +1,32 @@
 #!/bin/bash
 
-if [ ! -f config/geonature/geonature_config.toml ]; then
-    echo SECRET_KEY = \"$(openssl rand -hex 16)\" > config/geonature/geonature_config.toml
+set -a && source .env && set +a
+
+if [ ! -d "$GEONATURE_CONFIG_DIR" ]; then
+    mkdir -p "$GEONATURE_CONFIG_DIR"
 fi
-if [ ! -f config/usershub/config.py ]; then
-    echo SECRET_KEY = \"$(openssl rand -hex 16)\" > config/usershub/config.py
+
+if [ ! -d "$GEONATURE_DATA_DIR" ]; then
+    mkdir -p "$GEONATURE_DATA_DIR"
+    mkdir -p "$GEONATURE_DATA_DIR/custom"
+    mkdir -p "$GEONATURE_DATA_DIR/media"
 fi
-touch config/geonature/dashboard_config.toml
-touch config/geonature/exports_config.toml
-touch config/geonature/monitorings_config.toml
-touch config/geonature/import_config.toml
+
+if [ ! -f "$GEONATURE_CONFIG_DIR/geonature/geonature_config.toml" ]; then
+    mkdir -p "$GEONATURE_CONFIG_DIR/geonature"
+    echo "SECRET_KEY = \"$(openssl rand -hex 16)\"" > "$GEONATURE_CONFIG_DIR/geonature/geonature_config.toml"
+fi
+
+if [ ! -f "$GEONATURE_CONFIG_DIR/usershub/config.py" ]; then
+    mkdir -p "$GEONATURE_CONFIG_DIR/usershub"
+    echo "SECRET_KEY = \"$(openssl rand -hex 16)\"" > "$GEONATURE_CONFIG_DIR/usershub/config.py"
+fi
+
+if [ ! -d "$GEONATURE_CONFIG_DIR/traefik" ]; then
+    mkdir -p "$GEONATURE_CONFIG_DIR/traefik/certs"
+fi
+
+touch "$GEONATURE_CONFIG_DIR/geonature/dashboard_config.toml"
+touch "$GEONATURE_CONFIG_DIR/geonature/exports_config.toml"
+touch "$GEONATURE_CONFIG_DIR/geonature/monitorings_config.toml"
+touch "$GEONATURE_CONFIG_DIR/geonature/import_config.toml"
