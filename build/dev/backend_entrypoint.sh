90160a12f2b510aaed01ce8aa02f9f36b29477d7
Christophe Ramet <christophe.ramet@mnhn.fr>
2025-09-01 10:37:19 +0200

    feat: Changed backend entrypoint to prompt info, to accelerate it in install_db and so i doesn't print error log

--- build/dev/backend_entrypoint.sh
+++ build/dev/backend_entrypoint.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
-for module in $(ls -d /sources/gn_*/ | sort);   do \
-    [ -d "$module" ] &&  geonature install-gn-module "$module" --build false ; \
-done
+if [ "${GEONATURE_SKIP_POPULATE_DB}" != "true" ]; then
+    echo "Modules to install: $(ls -d /sources/gn_*/ | sort)"
+    for module in $(ls -d /sources/gn_*/ | sort); do
+        [ -d "$module" ] && pip install -e "$module"
+    done
+fi
 
 . /entrypoint.sh
