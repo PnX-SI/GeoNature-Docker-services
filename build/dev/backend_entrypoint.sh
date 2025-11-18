e84382ea1eaab519c426231b8df8110a6ea50c18
Christophe Ramet <christophe.ramet@mnhn.fr>
2025-11-18 16:40:32 +0100

    chore: dev auto install GN dependencies

--- build/dev/backend_entrypoint.sh
+++ build/dev/backend_entrypoint.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
-if [ "${GEONATURE_SKIP_POPULATE_DB}" != "true" ]; then
-    echo "Modules to install: $(ls -d /sources/gn_*/ | sort)"
-    for module in $(ls -d /sources/gn_*/ | sort); do
-        [ -d "$module" ] && pip install -e "$module"
-    done
-fi
+echo "Modules to install: $(ls -d /sources/gn_*/ | sort)"
+for module in $(ls -d /sources/gn_*/ | sort); do
+    [ -d "$module" ] && pip install -e "$module"
+done
+echo "Dependencies to install: $(ls -d /sources/GeoNature/backend/dependencies/* | sort)"
+for dependencies in $(ls -d /sources/GeoNature/backend/dependencies/* | sort); do
+    [ -d "$dependencies" ] && pip install -e "$dependencies"
+done
 
 . /entrypoint.sh
