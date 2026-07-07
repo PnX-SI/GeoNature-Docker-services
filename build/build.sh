da181e15b0aacdded405dcbcdc214f2e8cc9d867
cramet <christophe.ramet@mnhn.fr>
2026-07-07 10:58:43 +0200

    fix: Corrected some error and user Geonature develop (waiting for fix on flask-admin)

--- build/build.sh
+++ build/build.sh
@@ -2,15 +2,15 @@
 
 set -x
 set -o nounset
-
+set -e
 # It is necessary to build base-* images before building final images, and compose does not support build
 # dependencies yet (solved with additional_contexts in compose 2.36.2 - not in Debian 13).
 
-# docker-compose-build must come first, as dev file (if present in COMPOSE_FILE) override build sections
-export COMPOSE_FILE=docker-compose-build.yml:$(docker compose config --environment | grep COMPOSE_FILE || echo "docker-compose.yml")
+# docker-compose.build must come first, as dev file (if present in COMPOSE_FILE) override build sections
+export COMPOSE_FILE=docker-compose.build.yml:$(docker compose config --environment | grep COMPOSE_FILE | awk -F= '{print $2}' || echo "docker-compose.yml")
 
 docker compose build base-backend
-docker compose build geonature-install-db # requires base-backend
+docker compose build geonature-backend # requires base-backend
 
 docker compose build base-frontend-source
 docker compose build base-frontend-nginx
